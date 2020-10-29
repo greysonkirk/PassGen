6788ae27270f14e98ff48c35da957ab3ce5697fe
Greyson Kirk <greysonkirk@yahoo.com>
2020-10-29 08:27:39 -0500

    added comments in js code

--- Assets/script.js
+++ Assets/script.js
@@ -8,15 +8,16 @@ let specChar = ["!@#$%^><&*()?"];
 
 function generatePassword() {
     // user prompts 
+    // get character limit and make sure its between 8 and 128 
     let charLim = prompt("Select character amount between 8 and 128?");
     while (isNaN(charLim) || charLim < 8 || charLim > 128) charLim = Number(prompt("Length MUST be between 8-128 character!!!"));
 
-
+    // confirming what character type user wants 
     let lowWant = confirm("Do you want lowercase characters?");
     let upWant = confirm("Do you want uppercase characters?");
     let specWant = confirm("Do you want special characters?");
     let numWant = confirm("Do you want number characters?");
-    // check user input
+    // check user input and make sure at least one option was chosen
     while (!lowWant && !upWant && !numWant && !specWant) {
         alert("You MUST choose at least one character type!!!")
         lowWant = confirm("Do you want lowercase characters?");
@@ -25,20 +26,20 @@ function generatePassword() {
         numWant = confirm("Do you want number characters?");
     }
 
-    // console.log(criteria)
+    // setting password variables
     var passRes = "";
     var wanted = [];
+    // adding characters the user chose to wanted variable
     if (lowWant) wanted += (lowChar)
     if (upWant) wanted += (capChar)
     if (specWant) wanted += (specChar)
     if (numWant) wanted += (numChar)
-
+        //for loop looping through wanted string the selected character amount and adding random characters to passRes variable 
     for (let i = 0; i < charLim; i++) {
         resChar = wanted[Math.floor(Math.random() * wanted.length)];
         passRes += resChar;
     }
-    console.log(passRes)
-    console.log(wanted)
+    //setting the password text in the text area with id password and setting passwordText to the passRes variable 
     var passwordText = document.querySelector("#password");
     passwordText.value = passRes
 }
@@ -46,16 +47,6 @@ function generatePassword() {
 
 // Assignment Code2012
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-
-//     var passwordText = document.querySelector("#password");
-//     passwordText.value = password;
-//     console.log(passwordText);
-
-// }
-
 
 
 // Add event listener to generate button
