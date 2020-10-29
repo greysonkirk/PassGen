eb3860bdab71b11a33bf58fb174826e283798f20
Greyson Kirk <greysonkirk@yahoo.com>
2020-10-28 19:10:35 -0500

    commiting js

--- Assets/script.js
+++ Assets/script.js
@@ -2,33 +2,51 @@
 let lowChar = ["abcdefghijklmnopqrstuvwxyz"];
 let capChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 let numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let specChar = ["!", "@", "#", "$", "%", "^", ">", "<", "&", "*", "(", ")", "?"]
-
-
-// user prompts 
-let charLim = prompt("Select character amount between 8 and 128?");
-while (isNaN(charLim) || charLim < 8 || charLim > 128) charLim = Number(prompt("Length MUST be between 8-128 character!!!"));
-
-
-let lowWant = confirm("Do you want lowercase characters?");
-let upWant = confirm("Do you want uppercase characters?");
-let specWant = confirm("Do you want special characters?");
-let numWant = confirm("Do you want number characters?");
-
-while (!lowWant && !upWant && !numWant && !specWant) {
-    alert("You MUST choose at least one character type!!!")
-    lowWant = confirm("Do you want lowercase characters?");
-    upWant = confirm("Do you want uppercase characters?");
-    specWant = confirm("Do you want special characters?");
-    numWant = confirm("Do you want number characters?");
-}
+let specChar = ["!@#$%^><&*()?"]
+    // let specChar = ["!", "@", "#", "$", "%", "^", ">", "<", "&", "*", "(", ")", "?"]
 
 
 
 
-for (let i = 0; i < charLim.value; i++) {
-    const value = charLim[i];
-    console.log()
+
+function generatePassword() {
+    // user prompts 
+    let charLim = prompt("Select character amount between 8 and 128?");
+    while (isNaN(charLim) || charLim < 8 || charLim > 128) charLim = Number(prompt("Length MUST be between 8-128 character!!!"));
+
+
+    let lowWant = confirm("Do you want lowercase characters?");
+    let upWant = confirm("Do you want uppercase characters?");
+    let specWant = confirm("Do you want special characters?");
+    let numWant = confirm("Do you want number characters?");
+    // check user input
+    while (!lowWant && !upWant && !numWant && !specWant) {
+        alert("You MUST choose at least one character type!!!")
+        lowWant = confirm("Do you want lowercase characters?");
+        upWant = confirm("Do you want uppercase characters?");
+        specWant = confirm("Do you want special characters?");
+        numWant = confirm("Do you want number characters?");
+    }
+    var password = "";
+    // let criteria = {
+    //     length: charLim,
+    //     lower: lowWant,
+    //     upper: upWant,
+    //     number: numWant,
+    //     special: specWant
+    // }
+    // console.log(criteria)
+    var wanted = [];
+    if (lowWant) password += (lowChar)
+    if (upWant) password += (capChar)
+    if (specWant) password += (specChar)
+    if (numWant) password += (numChar)
+
+    for (let i = 0; i <= charLim; i++) {
+        password = password[Math.floor(Math.random() * password.length)];
+
+    }
+    console.log(password);
 }
 
 
@@ -37,10 +55,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    var genPass = generatePassword();
     var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+    passwordText.value = genPass;
 
 }
 
